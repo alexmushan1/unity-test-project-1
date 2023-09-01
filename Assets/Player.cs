edcad735eb15b5743f822299b86fe8fe1d4f1335
Tony <legendmastertony@gmail.com>
2023-09-01 14:10:50 +0800

    Change Player component to a seperate one instead of inheriting Character

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -2,23 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : Character
+public class Player : MonoBehaviour
 {
 
-
+    public Character characterComponent;
     // Start is called before the first frame update
     void Start()
     {
-        base.Start();
     }
     // Update is called once per frame
     void Update()
     {
+        characterComponent = GetComponent<Character>();
         // Get the horizontal and vertical axis.
         // By default they are mapped to the arrow keys.
         // The value is in the range -1 to 1
-        float dx = Input.GetAxisRaw("Horizontal") * speed;
-        float dy = Input.GetAxisRaw("Vertical") * speed;
+        float dx = Input.GetAxisRaw("Horizontal") * characterComponent.speed;
+        float dy = Input.GetAxisRaw("Vertical") * characterComponent.speed;
 
         // Make it move 10 meters per second instead of 10 meters per frame...
         dx *= Time.deltaTime;
@@ -27,11 +27,11 @@ public class Player : Character
         transform.Translate(dx, dy, 0);
 
         var positionDelta = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-        grabPoint.transform.rotation = Quaternion.AngleAxis(Mathf.Atan2(positionDelta.y, positionDelta.x) * Mathf.Rad2Deg, Vector3.forward);
+        characterComponent.grabPoint.transform.rotation = Quaternion.AngleAxis(Mathf.Atan2(positionDelta.y, positionDelta.x) * Mathf.Rad2Deg, Vector3.forward);
     }
 
     void PickupWeapon(GameObject weapon)
     {
-        weaponManager.ChangeWeapon(weapon);
+        characterComponent.weaponManager.ChangeWeapon(weapon);
     }
 }
