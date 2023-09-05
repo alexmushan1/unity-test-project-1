51ef70ddd91a876280258ae50364270a7da0843a
Tony <legendmastertony@gmail.com>
2023-09-05 22:09:06 +0800

    Add thrust weapon

--- Assets/Components/Weapon.cs
+++ Assets/Components/Weapon.cs
@@ -4,7 +4,14 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
+    public enum WeaponType
+    {
+        Slash,
+        Thrust,
+        Ranged,
+    }
 
+    public WeaponType weaponType;
     public int damage = 10;
     public float attackCooldownSec = 0.2f;
     public int range = 5;
@@ -19,18 +26,35 @@ public class Weapon : MonoBehaviour
             return;
         }
         lastAttackTimeSec = Time.time;
-        if (projectilePrefab != null)
+        switch (weaponType)
         {
-            var firePoint = transform.Find("FirePoint");
-            var projectile = Instantiate(projectilePrefab, firePoint.position, equippedCharacter.transform.rotation);
-            var projectileComponent = projectile.GetComponent<Projectile>();
-            var characterComponent = equippedCharacter.GetComponent<Character>();
-            projectileComponent.damage = damage;
-            projectile.transform.rotation = characterComponent.grabPointTransform.rotation;
-            projectile.GetComponent<Rigidbody2D>().AddForce(
-                characterComponent.grabPointTransform.up * projectileComponent.speed
-            );
-            // projectile.GetComponent<Rigidbody2D>().AddForce(new Vector2(100, 0));
+            case WeaponType.Slash:
+                break;
+            case WeaponType.Thrust:
+                ThrustAttack();
+                break;
+            case WeaponType.Ranged:
+                RangedAttack();
+                break;
         }
+    }
+
+    void ThrustAttack()
+    {
+
+    }
+
+    void RangedAttack()
+    {
+        var firePoint = transform.Find("FirePoint");
+        var projectile = Instantiate(projectilePrefab, firePoint.position, equippedCharacter.transform.rotation);
+        var projectileComponent = projectile.GetComponent<Projectile>();
+        var characterComponent = equippedCharacter.GetComponent<Character>();
+        projectileComponent.damage = damage;
+        projectile.transform.rotation = characterComponent.grabPointTransform.rotation;
+        projectile.GetComponent<Rigidbody2D>().AddForce(
+            characterComponent.grabPointTransform.up * projectileComponent.speed
+        );
+        // projectile.GetComponent<Rigidbody2D>().AddForce(new Vector2(100, 0));
     }
 }
