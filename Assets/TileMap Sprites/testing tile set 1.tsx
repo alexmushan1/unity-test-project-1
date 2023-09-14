9fb85f5c7a236237c62aba9a992b0b29c5e1e0b4
alexmushan1 <114463341+alexmushan1@users.noreply.github.com>
2023-09-14 00:14:24 -0700

    tile map implemented

---
+++ Assets/TileMap Sprites/testing tile set 1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="testing tile set 1" tilewidth="128" tileheight="128" tilecount="49" columns="7">
+ <image source="tile set attempt.png" width="1000" height="1000"/>
+</tileset>
